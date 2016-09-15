346ddf09774e4a67a7973babfc54c3bce7d46c55
alexv7 <alexvarjabedian7@gmail.com>
2016-09-15 01:29:28 -0400

    changed table name to spree_contacts, as i got an error 'Unable to autoload constant Spree::ContactsController'

--- db/migrate/20160915043305_create_contacts.rb
+++ db/migrate/20160915043305_create_contacts.rb
@@ -1,6 +1,6 @@
 class CreateContacts < ActiveRecord::Migration
   def change
-    create_table :contacts do |t|
+    create_table :spree_contacts do |t|
       t.string :name
       t.string :email
       t.string :phone
