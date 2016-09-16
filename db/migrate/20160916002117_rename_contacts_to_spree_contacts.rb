188697c94479bafbe33ee82f0fd4d3e1898c51e0
alexv7 <alexvarjabedian7@gmail.com>
2016-09-15 20:39:10 -0400

    syntax for renaming table

--- db/migrate/20160916002117_rename_contacts_to_spree_contacts.rb
+++ db/migrate/20160916002117_rename_contacts_to_spree_contacts.rb
@@ -1,4 +1,4 @@
-class RenameContactToNamespace < ActiveRecord::Migration
+class RenameContactToSpreeContacts < ActiveRecord::Migration
   def change
     rename_table :contacts, :spree_contacts
   end
