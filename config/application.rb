cbeed8bd2b79083fc15c3563a9a68ba94aad83d0
alexv7 <alexvarjabedian7@gmail.com>
2016-09-17 19:54:24 -0400

    trying to make sending emails in production work

--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,15 @@ module Spree
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.default :charset => "utf-8"
     config.action_mailer.default content_type: "text/html"
-    # config.action_mailer.default_url_options = { host: 'alexxmystore.herokuapp.com' }
+    host = 'alexmystore.herokuapp.com'
+    config.action_mailer.default_url_options = { host: host }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      address:              "smtp.gmail.com",
-      port:                 587,
-      domain:               'mail.google.com',
-      user_name:            'centerfortoys@gmail.com',
-      password:             'Centerfortoys4$',
+      address:              "smtp.sendgrid.net",
+      port:                 '587',
+      domain:               'heroku.com',
+      user_name:            ENV['SENDGRID_USERNAME'],
+      password:             ENV['SENDGRID_PASSWORD'],
       authentication:       :plain,
       enable_starttls_auto: true 
     }
