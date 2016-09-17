a5f8326185344590439a77435bd72520784cf603
alexv7 <alexvarjabedian7@gmail.com>
2016-09-17 16:58:02 -0400

    testing to see env variables without quotes around them in contact_mailer.rb

--- app/mailers/spree/contact_mailer.rb
+++ app/mailers/spree/contact_mailer.rb
@@ -1,11 +1,11 @@
 module Spree
   class ContactMailer < Spree::BaseMailer
-    default from: "#{ENV['TOY_EMAIL']}"
+    default from: ENV['TOY_EMAIL']
     # layout 'mailer'
 
     def contact_email(contact)
       @contact = contact
-      mail(to: "#{ENV['TOY_EMAIL']}", subject: 'Thank you for contacting Center for Toys')
+      mail(to: ENV['TOY_EMAIL'], subject: 'Thank you for contacting Center for Toys')
     end
   end
 end
