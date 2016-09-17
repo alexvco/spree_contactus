b1a3bc25d8070409a69d64968debdbd30a6873aa
alexv7 <alexvarjabedian7@gmail.com>
2016-09-17 19:34:49 -0400

    production...

--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,14 @@ module Spree
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.default :charset => "utf-8"
     config.action_mailer.default content_type: "text/html"
-    config.action_mailer.default_url_options = { host: 'alexxmystore.herokuapp.com' }
+    # config.action_mailer.default_url_options = { host: 'alexxmystore.herokuapp.com' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      address:              "smtp.sendgrid.net",
+      address:              "smtp.gmail.com",
       port:                 587,
-      user_name:            ENV['SENDGRID_USERNAME'],
-      password:             ENV['SENDGRID_PASSWORD'],
-      domain:               'heroku.com',
+      domain:               'mail.google.com',
+      user_name:            'centerfortoys@gmail.com',
+      password:             'Centerfortoys4$',
       authentication:       :plain,
       enable_starttls_auto: true 
     }
