9ca79e3cfe5c97c1661adccf4926a4bcb20bc518
alexv7 <alexvarjabedian7@gmail.com>
2016-09-17 18:35:39 -0400

    needed to put the host in quotes for it to work

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ module Spree
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.default :charset => "utf-8"
     config.action_mailer.default content_type: "text/html"
-    config.action_mailer.default_url_options = { host: alexxmystore.herokuapp.com }
+    config.action_mailer.default_url_options = { host: 'alexxmystore.herokuapp.com' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       address:              "smtp.sendgrid.net",
