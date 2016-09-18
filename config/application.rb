0a9190c3ace6369ab5854c09ea6e1c4bbd100070
alexv7 <alexvarjabedian7@gmail.com>
2016-09-18 17:01:42 -0400

    now trying with enable mail delivery box unchecked and seeing if it will use the spree_contactus extensions config/application.rb smtp settings

--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,16 @@ module Spree
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.default :charset => "utf-8"
     config.action_mailer.default content_type: "text/html"
-    # config.action_mailer.default_url_options = { host: ENV['HOST'] }
+    config.action_mailer.default_url_options = { host: ENV['HOST'] }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      # address:              "smtp.gmail.com",
-      # port:                 '587',
-      # domain:               'mail.google.com',
-      # user_name:             ENV['TOY_EMAIL'],
-      # password:              ENV['TOY_PASS'],
-      # authentication:       :plain,
-      # enable_starttls_auto: true 
+      address:              "smtp.gmail.com",
+      port:                 '587',
+      domain:               'mail.google.com',
+      user_name:             ENV['TOY_EMAIL'],
+      password:              ENV['TOY_PASS'],
+      authentication:       :plain,
+      enable_starttls_auto: true 
     }
   end
 end 
