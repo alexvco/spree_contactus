aca088e6ccdcf642865017ae9bd3a05090c0c580
alexv7 <alexvarjabedian7@gmail.com>
2016-09-18 13:55:31 -0400

    heroku and gmail

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ module Spree
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.default :charset => "utf-8"
     config.action_mailer.default content_type: "text/html"
-    # host = 'alexmystore.herokuapp.com'
-    # config.action_mailer.default_url_options = { host: host }
+    host = 'alexmystore.herokuapp.com'
+    config.action_mailer.default_url_options = { host: host }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       address:              "smtp.gmail.com",
