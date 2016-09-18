cda4bb3c88eb46cd8963892f03ac9f3469f03a39
alexv7 <alexvarjabedian7@gmail.com>
2016-09-18 18:08:20 -0400

    array emails, reply_to and bcc all work, that means that the only thing that i havent been able to configure  at this point is the smtp settings (config/application.rb) which is all being handled by the admin interface at the moment, which is ok for now... google rails using two smtp servers for possibly figuring out how to handle that or check in the spree source code, but for now this is ok

--- app/mailers/spree/contact_mailer.rb
+++ app/mailers/spree/contact_mailer.rb
@@ -5,8 +5,7 @@ module Spree
 
     def contact_email(contact)
       @contact = contact
-      array_email = ['centerfortoys@gmail.com', 'playm3n@hotmail.com']
-      mail(to: array_email, subject: 'Thank you for contacting Center for Toys', bcc: "v.a_l_e_x@yahoo.com", reply_to: "alexvarjabedian7@gmail.com")
+      mail(to: 'centerfortoys@gmail.com', subject: 'Thank you for contacting Center for Toys')
     end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -3,22 +3,23 @@ require 'action_mailer'
 
 module Spree
   class ContactMailer < Spree::BaseMailer
+    #This is all being done from the store backend...
 
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.default :charset => "utf-8"
-    config.action_mailer.default content_type: "text/html"
-    config.action_mailer.default_url_options = { host: ENV['HOST'] }
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address:              "smtp.gmail.com",
-      port:                 '587',
-      domain:               'mail.google.com',
-      user_name:             ENV['TOY_EMAIL'],
-      password:              ENV['TOY_PASS'],
-      authentication:       :plain,
-      enable_starttls_auto: true 
-    }
+    # config.action_mailer.perform_deliveries = true
+    # config.action_mailer.raise_delivery_errors = true
+    # config.action_mailer.default :charset => "utf-8"
+    # config.action_mailer.default content_type: "text/html"
+    # config.action_mailer.default_url_options = { host: ENV['TOY_HOST'] }
+    # config.action_mailer.delivery_method = :smtp
+    # config.action_mailer.smtp_settings = {
+    #   address:              "smtp.gmail.com",
+    #   port:                 '587',
+    #   domain:               'mail.google.com',
+    #   user_name:             ENV['TOY_EMAIL'],
+    #   password:              ENV['TOY_PASS'],
+    #   authentication:       :plain,
+    #   enable_starttls_auto: true 
+    # }
   end
 end 
    
