d3ab560cd54699168acb8cf71041701fcb54d7fe
alexv7 <alexvarjabedian7@gmail.com>
2016-09-18 13:46:38 -0400

    back to gmail in dev

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,15 @@ module Spree
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.default :charset => "utf-8"
     config.action_mailer.default content_type: "text/html"
-    host = 'alexmystore.herokuapp.com'
-    config.action_mailer.default_url_options = { host: host }
+    # host = 'alexmystore.herokuapp.com'
+    # config.action_mailer.default_url_options = { host: host }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      address:              "smtp.sendgrid.net",
+      address:              "smtp.gmail.com",
       port:                 '587',
-      domain:               'heroku.com',
-      user_name:            ENV['SENDGRID_USERNAME'],
-      password:             ENV['SENDGRID_PASSWORD'],
+      domain:               'mail.google.com',
+      user_name:            'centerfortoys@gmail.com',
+      password:             'Centerfortoys4$',
       authentication:       :plain,
       enable_starttls_auto: true 
     }
